e3661e06ca4604e1740d9b5f4ee7929150e6d859
Neyte <neya-liz@mail.ru>
2018-12-09 22:09:27 +0300

    commit 2

--- Lesson 2/MonteCarlo.py
+++ Lesson 2/MonteCarlo.py
@@ -1,5 +1,6 @@
 import random
 random.seed(42)
+print(end='\n')
 
 #   y = -x^2 + 4
 S_sq = 16
@@ -12,4 +13,4 @@ for i in range(n):
     y = random.uniform(0, 4)
     if y <= -(x-2)*(x-2) + 4:
         N_p += 1
-print("S_p = {0}".format(S_sq * N_p / n))
+print('S_p = {0:.2f}'.format(S_sq * N_p / n))
